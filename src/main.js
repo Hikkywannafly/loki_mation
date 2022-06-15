cde4465e40137a9622a54bbc99579a679b122975
Hikkywannafly <76279331+TamNguyenS@users.noreply.github.com>
2022-06-15 21:19:36 +0700

    async

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 "use strict";
-let music = "music/music.mp3";
 const fonts = [
   "Amatic SC",
   "Fredericka the Great",
@@ -10,23 +9,35 @@ const fonts = [
   "Galindo",
   "Princess Sofia",
 ];
+let music = "music/music.mp3";
 let count = 0;
 let elemets = document.querySelectorAll(".element");
-
-const ramdomFonts = function () {
-  elemets.forEach((elemet) => {
-    let ramdomFontsIndex = Math.floor(Math.random() * fonts.length);
-    let ramdomFonts = fonts[ramdomFontsIndex];
-    elemet.style.fontFamily = ramdomFonts;
-  });
-};
-const loopTime = setInterval(function () {
-  ramdomFonts();
-}, 400);
 let audio = document.createElement("audio");
-audio.autoplay = true;
-audio.load();
-audio.addEventListener("load",function () {
-    audio.play();
-},true);
-audio.src = music;
+
+const animationRun = () =>{
+  const ramdomFonts = () => {
+    elemets.forEach((elemet) => {
+      let ramdomFontsIndex =  Math.floor(Math.random() * fonts.length);
+      let ramdomFonts = fonts[ramdomFontsIndex];
+      elemet.style.fontFamily = ramdomFonts;
+    });
+  };
+
+  const loopTime =  setInterval(function () {
+    ramdomFonts();
+  }, 400);
+}
+const audioRun = () => {
+  audio.autoplay = true;
+  audio.load();
+  audio.addEventListener("load", () =>{audio.play()},true);
+  audio.src = music;
+}
+
+const finalRun = async () => {
+  await audioRun();
+   animationRun();
+ 
+};
+finalRun();
+
